2bb4eba42175de2ed7a86c5965829134572b0465
Jonas Wagner <jonas.wagner@epfl.ch>
2014-02-07 16:09:17 +0100

    Add a list of channels, and some commands

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -14,9 +14,25 @@ def send(frame):
     p = packet.DmxPacket(frame)
     sock.sendto(p.encode(), ('<broadcast>', packet.ARTNET_PORT))
 
-f_on = dmx.Frame([255]*512)
-f_off = dmx.Frame([0]*512)
+#f_on = dmx.Frame([255]*512)
+#f_off = dmx.Frame([0]*512)
+# send(f_on)
+# time.sleep(3.0)
+# send(f_off)
 
-send(f_on)
-time.sleep(3.0)
-send(f_off)
+global_frame = dmx.Frame([0] * 512)
+def set_channel(channel, value):
+    global_frame[channel] = value
+    for i in range(3):
+        send(global_frame)
+
+# Channels
+ch_color = 192
+ch_gobo_rotate = 193
+ch_gobo = 194
+ch_dimmer = 195
+ch_pan = 196
+ch_tilt = 197
+ch_effect = 198
+
+
